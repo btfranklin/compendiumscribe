2e73af8b66890540ea709b3b1b6a957cc25a30ba
B.T. Franklin <brandon.franklin@gmail.com>
2024-11-24 00:45:03 -0700

    Checkpoint of progress toward the new workflow, mostly working

--- src/compendiumscribe/create_compendium.py
+++ src/compendiumscribe/create_compendium.py
@@ -9,11 +9,12 @@ from dotenv import load_dotenv
 from promptdown import StructuredPrompt
 from pickled_pipeline import Cache
 
-from .model import Domain, Topic
+from .model import Domain, Topic, Concept
 
 cache = Cache()
 
 
+# Step 1: Provide Domain, which is what the Compendium will be about.
 def create_compendium(domain: str) -> Domain:
     """
     Main pipeline for creating a compendium from a given domain.
@@ -50,45 +51,59 @@ def create_compendium(domain: str) -> Domain:
 
     print(f"{Back.BLUE} CREATING COMPENDIUM ")
 
-    # Step 1: Enhance the provided domain of expertise
+    # Step 2: Enhance the provided domain of expertise
     enhanced_domain = enhance_domain(llm_client, domain)
 
     # Create the Domain object
     compendium_domain = Domain(name=enhanced_domain)
 
-    # Step 2: Create a comprehensive list of Topics to Research
+    # Step 3: Create a comprehensive list of Topics to Research
     topics_to_research = create_topics_to_research(llm_client, enhanced_domain)
 
-    # For each Topic
-    questions_and_answers: list[list[tuple[str, str]]] = []
-    for topic in topics_to_research:
-        # Step 3: Create a collection of Research Questions
+    # Step 4: For each Topic to Research...
+    for topic_to_research in topics_to_research:
+
+        # Step 4.1: Create the Topic object
+        topic = Topic(name=topic_to_research)
+
+        # Step 4.2: Create a collection of Research Questions
         research_questions = create_research_questions(
-            llm_client, enhanced_domain, topic
+            llm_client, enhanced_domain, topic_to_research
         )
-        topic_questions_and_answers: list[tuple[str, str]] = []
-        # Step 4: Answer each Research Question
+
+        # Step 4.3: For each of the Research Questions...
         for question in research_questions:
+
+            # Step 4.3.1: Answer the Research Question
             answer = answer_research_question(online_llm_client, question)
             if not answer:
                 print(f"{Fore.YELLOW}Failed to answer question: {question}")
                 continue
-            # Record the question and answer
-            topic_questions_and_answers += [(question, answer)]
-        questions_and_answers.append(topic_questions_and_answers)
 
-    # Step 6: Generate a structured list of Topics from Research Findings
-    # topic_names = generate_topics_from_research_findings(
-    #     llm_client, combined_research_findings
-    # )
+            # Step 4.3.2: Use the answer content to generate a Concept Name
+            concept_name = generate_concept_name_from_answer(llm_client, answer)
 
-    # Step 7: Generate detailed Topics
-    # for topic_name in topic_names:
-    #     topic = generate_topic(llm_client, topic_name, combined_research_findings)
-    #     compendium_domain.topics.append(topic)
+            # Step 4.3.3: Create a Concept in the Topic
+            concept = Concept(name=concept_name, content=answer)
+            topic.concepts.append(concept)
 
-    # Step 8: Generate Domain Summary
-    compendium_domain.summary = generate_domain_summary(llm_client, compendium_domain)
+            # Step 4.3.4: Generate all of the metadata for the Concept
+
+            # Additional Questions
+            concept.questions.append(question)
+            additional_questions = create_additional_concept_questions(
+                llm_client, answer, question
+            )
+            concept.questions.extend(additional_questions)
+
+            # Keywords
+            # concept.keywords.append(concept_name)
+
+            # Prerequisites
+            # concept.prerequisites.append(topic_to_research)
+
+    # Step 5: Generate Domain Summary
+    # compendium_domain.summary = generate_domain_summary(llm_client, compendium_domain)
 
     return compendium_domain
 
@@ -98,7 +113,7 @@ def enhance_domain(llm_client: OpenAI, domain: str) -> str:
     model_name = os.environ.get("ENHANCE_DOMAIN_LLM", "gpt-4o")
     structured_prompt = StructuredPrompt.from_package_resource(
         package="compendiumscribe.prompts",
-        resource_name="1_enhance_domain.prompt.md",
+        resource_name="2_enhance_domain.prompt.md",
     )
     structured_prompt.apply_template_values({"domain": domain})
     messages = structured_prompt.to_chat_completion_messages()
@@ -118,7 +133,7 @@ def create_topics_to_research(llm_client: OpenAI, domain: str) -> list[str]:
     model_name = os.environ.get("CREATE_TOPICS_TO_RESEARCH_LLM", "gpt-4o")
     structured_prompt = StructuredPrompt.from_package_resource(
         package="compendiumscribe.prompts",
-        resource_name="2_create_topics_to_research.prompt.md",
+        resource_name="3_create_topics_to_research.prompt.md",
     )
     structured_prompt.apply_template_values(
         {
@@ -126,29 +141,29 @@ def create_topics_to_research(llm_client: OpenAI, domain: str) -> list[str]:
         }
     )
     messages = structured_prompt.to_chat_completion_messages()
+
     response = llm_client.chat.completions.create(
         model=model_name,
         messages=messages,
         max_tokens=1000,
         temperature=0.7,
-        response_format=list[str],
     )
-    areas_text = response.choices[0].message.content.strip()
+    topics_text = response.choices[0].message.content.strip()
     try:
         # If the text is wrapped in ```json...``` format, remove those indicators
-        if areas_text.startswith("```json") and areas_text.endswith("```"):
-            areas_text = areas_text[7:-3]
+        if topics_text.startswith("```json") and topics_text.endswith("```"):
+            topics_text = topics_text[7:-3]
 
         # Parse the JSON response
-        areas_of_research = json.loads(areas_text)
-        if not isinstance(areas_of_research, list):
-            raise ValueError("Areas of Research should be a list.")
+        topics_to_research = json.loads(topics_text)
+        if not isinstance(topics_to_research, list):
+            raise ValueError("Topics to Research should be a list.")
     except (json.JSONDecodeError, ValueError) as e:
-        print(f"{Fore.RED}Error parsing Areas of Research: {e}")
+        print(f"{Fore.RED}Error parsing Topics to Research: {e}")
         sys.exit(1)
 
-    print(f"{Fore.BLUE}Areas of Research:{Fore.RESET} {areas_of_research}")
-    return areas_of_research
+    print(f"{Fore.BLUE}Topics to Research:{Fore.RESET} {topics_to_research}")
+    return topics_to_research
 
 
 @cache.checkpoint(exclude_args=["llm_client"])
@@ -172,7 +187,7 @@ def create_research_questions(llm_client: OpenAI, domain: str, topic: str) -> li
     number_of_questions = os.environ.get("NUMBER_OF_QUESTIONS_PER_AREA", "10")
     structured_prompt = StructuredPrompt.from_package_resource(
         package="compendiumscribe.prompts",
-        resource_name="3_create_research_questions.prompt.md",
+        resource_name="4_2_create_research_questions.prompt.md",
     )
     structured_prompt.apply_template_values(
         {
@@ -183,21 +198,11 @@ def create_research_questions(llm_client: OpenAI, domain: str, topic: str) -> li
     )
     messages = structured_prompt.to_chat_completion_messages()
 
-    # Define a class that will allow a response in this format:
-    # [
-    #    {"number": 1, "question": "First question"},
-    #    {"number": 2, "question": "Second question"},
-    # ]
-    class Question:
-        number: int
-        question: str
-
     response = llm_client.chat.completions.create(
         model=model_name,
         messages=messages,
         max_tokens=1000,
         temperature=0.7,
-        response_format=list[Question],
     )
     questions_text = response.choices[0].message.content.strip()
     try:
@@ -205,14 +210,22 @@ def create_research_questions(llm_client: OpenAI, domain: str, topic: str) -> li
         if questions_text.startswith("```json") and questions_text.endswith("```"):
             questions_text = questions_text[7:-3]
 
-        # Parse the JSON response
+        # Parse the JSON response, which should contain a list of objects that looks like this:
+        # [
+        #    {"number": 1, "question": "First question"},
+        #    {"number": 2, "question": "Second question"},
+        #    ...
+        # ]
         questions_list = json.loads(questions_text)
         if not isinstance(questions_list, list):
             raise ValueError("Research Questions should be a list of objects.")
         questions = []
-        for item in questions_list:
-            if "question" in item:
-                questions.append(item["question"].strip())
+        for numbered_question in questions_list:
+            if "question" in numbered_question:
+
+                # Get the question string from the object
+                question = numbered_question["question"].strip()
+                questions.append(question)
             else:
                 # Warn if the question is missing
                 print(
@@ -227,7 +240,9 @@ def create_research_questions(llm_client: OpenAI, domain: str, topic: str) -> li
         print(f"{Fore.RED}Error parsing Research Questions for topic '{topic}': {e}")
         questions = []
 
-    print(f"{Fore.BLUE}Research Questions for '{topic}':{Fore.RESET} {questions}")
+    print(f"{Fore.BLUE}Research Questions for '{topic}':")
+    for question in questions:
+        print(f" - {question}")
 
     return questions
 
@@ -245,7 +260,7 @@ def answer_research_question(online_llm_client: OpenAI, question: str) -> str:
     )
     structured_prompt = StructuredPrompt.from_package_resource(
         package="compendiumscribe.prompts",
-        resource_name="4_research_and_generate_answer.prompt.md",
+        resource_name="4_3_1_research_and_generate_answer.prompt.md",
     )
     structured_prompt.apply_template_values({"question": question})
     messages = structured_prompt.to_chat_completion_messages()
@@ -261,6 +276,78 @@ def answer_research_question(online_llm_client: OpenAI, question: str) -> str:
     except Exception as e:
         print(f"{Fore.RED}Error answering question '{question}': {e}")
         return ""
+
+
+@cache.checkpoint(exclude_args=["llm_client"])
+def generate_concept_name_from_answer(llm_client: OpenAI, answer: str) -> str:
+    model_name = os.environ.get("GENERATE_CONCEPT_NAME_FROM_ANSWER_LLM", "gpt-4o")
+    structured_prompt = StructuredPrompt.from_package_resource(
+        package="compendiumscribe.prompts",
+        resource_name="4_3_2_generate_concept_name.prompt.md",
+    )
+    structured_prompt.apply_template_values({"answer": answer})
+    messages = structured_prompt.to_chat_completion_messages()
+    response = llm_client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        temperature=0.2,
+        max_tokens=100,
+    )
+    concept_name = response.choices[0].message.content.strip()
+    print(f"{Fore.BLUE}Concept Name:{Fore.RESET} {concept_name}")
+    return concept_name
+
+
+@cache.checkpoint(exclude_args=["llm_client"])
+def create_additional_concept_questions(
+    llm_client: OpenAI, answer: str, question: str
+) -> list[str]:
+    model_name = os.environ.get("CREATE_ADDITIONAL_CONCEPT_QUESTIONS_LLM", "gpt-4o")
+    structured_prompt = StructuredPrompt.from_package_resource(
+        package="compendiumscribe.prompts",
+        resource_name="4_3_3_create_additional_concept_questions.prompt.md",
+    )
+    structured_prompt.apply_template_values({"answer": answer, "question": question})
+    messages = structured_prompt.to_chat_completion_messages()
+    response = llm_client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        temperature=0.7,
+        max_tokens=1000,
+    )
+    additional_questions_text = response.choices[0].message.content.strip()
+
+    try:
+        # If the text is wrapped in ```json...``` format, remove those indicators
+        if additional_questions_text.startswith(
+            "```json"
+        ) and additional_questions_text.endswith("```"):
+            additional_questions_text = additional_questions_text[7:-3]
+
+        # Parse the JSON response
+        additional_questions_list = json.loads(additional_questions_text)
+        if not isinstance(additional_questions_list, list):
+            raise ValueError("Additional Questions should be a list of strings.")
+        additional_questions = []
+        for additional_question in additional_questions_list:
+            additional_questions.append(additional_question.strip())
+    except (json.JSONDecodeError, ValueError) as e:
+        print(f"{Fore.RED}Error parsing Additional Questions: {e}")
+        additional_questions = []
+
+    print(f"{Fore.BLUE}Additional Questions:")
+    for question in additional_questions:
+        print(f" - {question}")
+    return additional_questions
+
+
+# Everything below here is outdated and will be deleted later
+#
+#
+#
+#
+#
+#
 
 
 def generate_topics_from_research_findings(
