f4e34e9e1e2b764c0d78498aae455793514cfaca
B.T. Franklin <brandon.franklin@gmail.com>
2024-12-05 21:45:08 -0700

    Improved the naming for generated compendia

--- src/compendiumscribe/cli.py
+++ src/compendiumscribe/cli.py
@@ -1,7 +1,9 @@
 import pickle
+import re
 import sys
 import click
 from dotenv import load_dotenv
+from datetime import datetime
 import colorama.initialise as colorama
 
 from compendiumscribe.create_llm_clients import create_llm_clients
@@ -31,13 +33,32 @@ def main(domain: str):
     try:
         domain_object = research_domain(domain, llm_client, online_llm_client)
 
+        # Create a file-friendly name string based on the domain name, in the form:
+        # domain_name_with_underscores_2024-12-31
+        # by:
+        # 1. removing any non-alphanumeric characters
+        # 2. converting spaces to underscores
+        # 3. converting to lowercase
+        # 4. Removing any underscores from the beginning or end of the string
+        # 5. Removing any consecutive underscores
+        # 6. Removing any trailing underscores
+        # 7. Adding the date of creation (YYYY-MM-DD) to the end of the string, separated by a single underscore
+        file_friendly_domain_name = re.sub(r"[^a-zA-Z0-9]+", "_", domain).lower()
+        file_friendly_domain_name = re.sub(r"^_+|_+$", "", file_friendly_domain_name)
+        file_friendly_domain_name = re.sub(r"_{2,}", "_", file_friendly_domain_name)
+        file_friendly_domain_name = (
+            file_friendly_domain_name + "_" + datetime.now().strftime("%Y-%m-%d")
+        )
+
         # Save the domain to a file by pickling it
-        with open("compendium.pickle", "wb") as f:
+        pickle_filename = f"{file_friendly_domain_name}.compendium.pickle"
+        with open(pickle_filename, "wb") as f:
             pickle.dump(domain_object, f)
 
         # Save the entire domain_object to an XML file as well
+        xml_filename = f"{file_friendly_domain_name}.compendium.xml"
         xml_string = domain_object.to_xml_string()
-        with open("compendium.xml", "w") as f:
+        with open(xml_filename, "w") as f:
             f.write(xml_string)
 
     except Exception as e:
